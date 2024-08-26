981bed44a3cfef83339fa658c30a3b3941d3ce22
Yewon2ee <hyewon8685@naver.com>
2024-08-26 14:42:48 +0900

    feat - #1 프로젝트 초기 세팅

---
+++ src/test/java/landvibe/serverstudy/HsServerstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package landvibe.serverstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsServerstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
