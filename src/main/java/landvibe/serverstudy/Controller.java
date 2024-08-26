0ba527946f4083af10659886c452be95da7b4c54
Yewon2ee <hyewon8685@naver.com>
2024-08-26 15:29:05 +0900

    feat:증가하는 메소드 구현

--- src/main/java/landvibe/serverstudy/Controller.java
+++ src/main/java/landvibe/serverstudy/Controller.java
@@ -1,8 +1,21 @@
 package landvibe.serverstudy;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 public class Controller {
+
+    AtomicInteger num = new AtomicInteger(0);
+
+    @GetMapping("/incr")
+
+    public String incrCnt() {
+        num.getAndIncrement();
+        return "a 증가";
+    }
 }
+
